2e458b20e849b62e326007ca28def37f04409293
Santiago Ontanon <santi@CCIsanti01.local>
2017-02-26 17:56:30 -0500

    added the possibility to import data from an ARFF file

--- src/tests/DistanceVisualization.java
+++ src/tests/DistanceVisualization.java
@@ -26,10 +26,18 @@ import util.Pair;
  */
 public class DistanceVisualization {
 
+    public static class ARFFInformation {
+        List<String> featureNames = null;
+        double originalData[][] = null;
+        List<String> classLabels = null;
+    }
+    
+    
     public static void main(String args[]) throws IOException, Exception {
         List<Pair<String, DistanceMatrix>> matrices = new LinkedList<>();
         List<Pair<String, List<String>>> labels = new LinkedList<>();
         List<String> names = new LinkedList<>();       
+        ARFFInformation arff = null;
         boolean ThreeD = false;
         
         
@@ -42,6 +50,17 @@ public class DistanceVisualization {
                 String labelsFileName = args[i+1];
                 loadLabels(labelsFileName, labelsFileName, labels);                
                 i++;
+            } else if (args[i].equals("-arff")) {
+                String arffFile = args[i+1];
+                
+                arff = loadDataFromArff(arffFile);
+                DistanceMatrix m = euclideanFromARFF(arff);
+                matrices.add(new Pair<>("ARFF-euclidean-distance", m));
+                labels.add(new Pair<>("ARFF-class-labels",arff.classLabels));
+                if (names.isEmpty()) {
+                    for(String name:m.names) names.add(name);
+                }
+                i++;
             } else if (args[i].equals("-3d")) {
                 ThreeD = true;
             } else {
@@ -51,14 +70,15 @@ public class DistanceVisualization {
         }
         
         if (matrices.size()==0) {
-            System.out.println("No distance matrix specified!! Please specify at least one distance matrix as: '-DM <distance matrix file>");
-            System.exit(1);
-        } else {
+            if (arff==null) {
+                System.out.println("No distance matrix specified!! Please specify at least one distance matrix as: '-DM <distance matrix file>");
+                System.exit(1);
+            }
+        } 
+        
+        setIndexLabels("index", matrices, labels);
             
-            setIndexLabels("index", matrices, labels);
-            
-            createVisualization(new ArrayList<>(), matrices, labels, names, ThreeD);        
-        }
+        createVisualization(new ArrayList<>(), matrices, labels, names, arff, ThreeD);        
     }
 
 
@@ -66,10 +86,16 @@ public class DistanceVisualization {
                                            List<Pair<String, DistanceMatrix>> matrices, 
                                            List<Pair<String, List<String>>> labels, 
                                            List<String> names, 
+                                           ARFFInformation arff,
                                            boolean ThreeD) {
         if (ThreeD) {
             Visualization3DMultiViewGUI.SHOW_KEY = true;
-            Visualization3DMultiViewGUI w = new Visualization3DMultiViewGUI("Data Visualization", matrices, positions, labels, names, null, null, 1);
+            Visualization3DMultiViewGUI w;
+            if (arff==null) {
+                w = new Visualization3DMultiViewGUI("Data Visualization", matrices, positions, labels, names, null, null, 1);
+            } else {
+                w = new Visualization3DMultiViewGUI("Data Visualization", matrices, positions, labels, names, arff.featureNames, arff.originalData, 1);
+            }
 //            w.visualization.KEY_THRESHOLD = 1;
             w.setResizable(true);
             w.setVisible(true);
@@ -140,5 +166,78 @@ public class DistanceVisualization {
         }
         all_labels.add(new Pair<String, List<String>>(name, labels));
     }       
+    
+
+    private static ARFFInformation loadDataFromArff(String arffFile) throws Exception 
+    {
+        ARFFInformation ret = new ARFFInformation();
+        
+        BufferedReader br = new BufferedReader(new FileReader(arffFile));
+        ret.featureNames = new ArrayList<>();
+        ret.classLabels = new ArrayList<>();
+        List<double []> instances = new ArrayList<>();
+        
+        while(true){
+            String line = br.readLine();
+            if (line==null) break;
+            
+            if (line.startsWith("@ATTRIBUTE")) {
+                StringTokenizer st = new StringTokenizer(line);
+                st.nextToken();
+                String attribute = st.nextToken();
+                if (!attribute.equals("class")) {
+                    ret.featureNames.add(attribute);
+                    String dataType = st.nextToken();
+                    if (!dataType.equals("NUMERIC")) {
+                        throw new Exception("non numeric attributes are not supported at this point when importing ARFF files!");
+                    }
+                }
+            } else if (line.startsWith("@DATA")) {
+                break;
+            }
+        }
+        
+        while(true){
+            String line = br.readLine();
+            if (line==null) break;
+            StringTokenizer st = new StringTokenizer(line,",");
+            double instance[] = new double[ret.featureNames.size()];
+            for(int i = 0;i<instance.length;i++) {
+                instance[i] = Double.parseDouble(st.nextToken());
+            }
+            instances.add(instance);
+            ret.classLabels.add(st.nextToken());
+        }        
+        
+        ret.originalData = new double[instances.size()][ret.featureNames.size()];
+        for(int i = 0;i<ret.originalData.length;i++) {
+            for(int j = 0;j<ret.originalData[i].length;j++) {
+                ret.originalData[i][j] = instances.get(i)[j];
+            }
+        }
+        return ret;
+    }
+    
+    
+    public static DistanceMatrix euclideanFromARFF(ARFFInformation arff)
+    {
+        int n = arff.classLabels.size();
+        int nFeatures = arff.featureNames.size();
+        DenseDistanceMatrix m = new DenseDistanceMatrix(n);
+        for(int i = 0;i<n;i++) {
+            m.names[i] = "ARFFInstance" + i;
+            for(int j = i+1;j<n;j++) {
+                double d = 0;
+                for(int f = 0;f<nFeatures;f++) {
+                    double fd = arff.originalData[i][f] - arff.originalData[j][f];
+                    d+=fd*fd;
+                }
+                d = Math.sqrt(d);
+                m.getMatrix()[i][j] = d;
+                m.getMatrix()[j][i] = d;
+            }
+        }
+        return m;
+    }
 
 }
